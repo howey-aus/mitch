9b5d30d57f87d24eb476520d08f415694aa2b581
Dan Howe <dan@dhowe.org>
2018-04-22 17:25:34 +1000

    Tidy up install script.

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 set -e
 SCRIPT="https://raw.githubusercontent.com/howey-aus/mitch/master/mitch"
+DEST="/usr/local/bin"
 
 if [[ $(id -u) -ne 0 ]]; then
-  echo "This script requires root priveleges to install to /usr/local/bin."
+  echo "This script requires root priveleges to install to ${DEST}."
   exit 1
 fi
 
-mkdir -p "/usr/local/bin"
-curl -o "/usr/local/bin/mitch" "$SCRIPT"
-chmod +x "/usr/local/bin/mitch"
+mkdir -p "$DEST"
+curl -o "${DEST}/mitch" "$SCRIPT"
+chmod +x "${DEST}/mitch"
+echo "mitch successfully installed to ${DEST}."
